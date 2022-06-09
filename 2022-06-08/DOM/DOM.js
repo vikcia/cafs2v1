374a36750fbc6ed0f7619693e9585d9e92712d85
Viktoras <vikcia188@gmail.com>
2022-06-09 18:45:33 +0300

    DOM updated

--- 2022-06-08/DOM/DOM.js
+++ 2022-06-08/DOM/DOM.js
@@ -2,4 +2,4 @@ console.log(document.getElementById ('age-table'));
 console.log(document.getElementById('age'));
 console.log(document.querySelectorAll ('label'));
 console.log(document.getElementById('form').children[1]);
-console.log(document.getElementById('form').children[3]);
+console.log(document.getElementById('form').children[5]);
