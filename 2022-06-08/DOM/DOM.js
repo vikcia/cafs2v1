fde489595064958c235c83d91c6b9372101fc27b
Viktoras <vikcia188@gmail.com>
2022-06-09 18:32:49 +0300

    added DOM

---
+++ 2022-06-08/DOM/DOM.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById ('age-table'));
+console.log(document.getElementById('age'));
+console.log(document.querySelectorAll ('label'));
+console.log(document.getElementById('form').children[1]);
+console.log(document.getElementById('form').children[3]);
