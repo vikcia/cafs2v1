d347145aab12a94f1c42796f5dc00d8b80a1c9fe
Viktoras <vikcia188@gmail.com>
2022-06-23 18:40:15 +0300

    add 2022-06-23

---
+++ 2022-06-23/app.js
@@ -0,0 +1,9 @@
+let p1 = new Movie('Star Wars: Episode VII - The Force Awakens', '1983', 'J. J. Abrams', '306000000', '2064615817');
+let p2 = new Movie('Star Wars: Episode IX – The Rise of Skywalker', '2019', 'J. J. Abrams', '275000000', '1074144248');
+
+
+console.log(p1.getIntroduction());
+console.log(p1.getProfit());
+
+console.log(p2.getIntroduction());
+console.log(p2.getProfit());
